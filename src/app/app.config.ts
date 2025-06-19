b3a787274c0f932c147ef0f0fd3b5a9735d67e50
murtazaalmas <mmurtazaalmas@gmail.com>
2025-06-19 12:44:50 +0500

    create new components

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,7 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { NavbarComponent } from './navbar/navbar.component';
-import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 import { routes } from './app.routes';
 
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,25 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { SetUpComponent } from './set-up/set-up.component';
-import { AccountManagementComponent } from './account-management/account-management.component';
-import { BeautySpaCardComponent } from './Business/beauty-spa-card/beauty-spa-card.component';
-import { BusinessCardComponent } from './Business/business-card/business-card.component';
+import { HomeComponent } from './components/home/home.component';
+import { SetUpComponent } from './components/set-up/set-up.component';
+import { BeautySpaCardComponent } from './components/beauty-spa-card/beauty-spa-card.component';
+import { AccountManagementComponent } from './components/account-management/account-management.component';
+
+import { CarRentalsComponent } from './components/car-rentals/car-rentals.component';
+import { CarDealersComponent } from './components/car-dealers/car-dealers.component';
+import { AutoRepairComponent } from './components/auto-repair/auto-repair.component';
+import { ParkingComponent } from './components/parking/parking.component';
+import { OilChangeServiceComponent } from './components/oil-change-service/oil-change-service.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'login', component: HomeComponent },
+  { path: '', component: AccountManagementComponent },
+  { path: 'login', component: AccountManagementComponent },
   { path: 'home', component: HomeComponent },
   { path: 'set-up', component: SetUpComponent },
-  { path: 'business/beauty-in-dubai', component: BeautySpaCardComponent },
-  { path: 'business/car-in-dubai', component: BusinessCardComponent },
-  { path: '**', redirectTo: '' } 
+  { path: 'businesses/beauty-in-dubai', component: BeautySpaCardComponent },
+  { path: 'businesses/car-rentals-in-dubai', component: CarRentalsComponent },
+  { path: 'businesses/car-dealers-in-dubai', component: CarDealersComponent },
+  { path: 'businesses/auto-repair-in-dubai', component: AutoRepairComponent },
+  { path: 'businesses/parking-in-dubai', component: ParkingComponent },
+  { path: 'businesses/oil-change-service-in-dubai', component: OilChangeServiceComponent },
+  { path: '**', redirectTo: '' }
 ];
