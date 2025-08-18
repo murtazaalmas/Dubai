dcbb98dc82df3c1bd450b23b0fe55c175fb6d206
murtaza <murtaza.almas@ssasoft.com>
2025-08-18 17:17:56 +0500

    update ui

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { FooterComponent } from '../footer/footer.component';
@@ -13,10 +13,66 @@ import { CommonModule } from '@angular/common';
   styleUrl: './home.component.scss'
 })
 
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+
+  slides = [
+    {
+      heading: 'Custom <br /> Team Wear',
+      subtext: 'The Best in Sports Equipment',
+      buttons: ['Rugby Kit', 'Fitness Wear', 'Tennis Kit'],
+      image1: 'assets/images/images1.jpeg',
+      image2: 'assets/images/images2.jpeg'
+    },
+    {
+      heading: 'Peak Performance <br /> Gear',
+      subtext: 'Engineered for the Modern Athlete',
+      buttons: ['Running Shoes'],
+      image1: 'assets/images/images3.jpeg',
+      image2: 'assets/images/images4.jpeg'
+    },
+    {
+      heading: 'Find Your <br /> Strength',
+      subtext: 'Comfort and Style for Every Workout',
+      buttons: ['Yoga Mats', 'Dumbbells'],
+      image1: 'assets/images/images5.jpeg',
+      image2: 'assets/images/images6.jpeg'
+    }
+  ];
+
+  currentIndex = 0;
+  slideInterval: any;
 
   ngOnInit() {
+    this.startSlider();
+  }
 
+  ngOnDestroy() {
+    if (this.slideInterval) {
+      clearInterval(this.slideInterval);
+    }
+  }
+
+  resetSliderInterval() {
+    if (this.slideInterval) {
+      clearInterval(this.slideInterval);
+    }
+    this.startSlider();
+  }
+
+  prevSlide() {
+    this.currentIndex = (this.currentIndex > 0) ? this.currentIndex - 1 : this.slides.length - 1;
+    this.resetSliderInterval();
+  }
+
+  nextSlide() {
+    this.currentIndex = (this.currentIndex + 1) % this.slides.length;
+    this.resetSliderInterval();
+  }
+
+  startSlider() {
+    this.slideInterval = setInterval(() => {
+      this.currentIndex = (this.currentIndex + 1) % this.slides.length;
+    }, 3000); // Change slide every 3 seconds
   }
 
 
