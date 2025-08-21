275620d78d9e4e21b779567e60e780c8d92df0f2
murtaza <murtaza.almas@ssasoft.com>
2025-08-22 02:33:20 +0500

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
-import { WheelComponent } from './components/wheel/wheel.component';
-import { CartComponent } from './components/cart/cart.component';
-import { CheckoutComponent } from './components/checkout/checkout.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
-  { path: 'wheel', component: WheelComponent },
-  { path: 'cart', component: CartComponent },
-  { path: 'checkout', component: WheelComponent },
   { path: '**', redirectTo: 'home' }
 ];
