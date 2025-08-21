d9ea14ea2e8fbba83d211dbe44fb804f4ad4cfc1
murtaza <murtaza.almas@ssasoft.com>
2025-08-22 04:41:36 +0500

    card game 4

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { FooterComponent } from './components/footer/footer.component';
 
 import { routes } from './app.routes';
 
@@ -9,7 +7,5 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes),
-    NavbarComponent,
-    FooterComponent
   ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,7 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
 import { ColorGameComponent } from './components/color-game/color-game.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'home', component: HomeComponent },
+  { path: '', component: ColorGameComponent },
   { path: 'color-game', component: ColorGameComponent },
-  
 ];
